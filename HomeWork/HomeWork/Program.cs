73b8e704362b0ee1e270e0f4ceab497e075e0016
User <bbv1052@gmail.com>
2021-04-12 23:16:35 +0300

    Core project

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var homework = new Home();
+            var result = homework.InvokePriceCalculatiion();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
